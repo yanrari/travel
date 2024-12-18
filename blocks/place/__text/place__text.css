4e363b196863f9a291ead70fa6a163ca9e38ae6d
yanrari <yanaup924@gmail.com>
2024-12-19 00:09:06 +0300

    Create place__text.css

---
+++ blocks/place/__text/place__text.css
@@ -0,0 +1,23 @@
+.place__text {
+  grid-area: text;
+}
+@media screen and (max-width: 1023px) {
+  .place__text {
+  }
+}
+@media screen and (min-width: 768px) and (max-width: 1023px) {
+  .place__text {
+    min-width: 352px;
+  }
+}
+@media screen and (max-width: 425px) {
+  .place__text {
+    padding: 0 16px;
+    max-width: calc(100vw - 16px * 2);
+  }
+}
+@media screen and (max-width: 320px) {
+  .place__text {
+    max-width: 288px;
+  }
+}
